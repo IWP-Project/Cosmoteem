aa95bbae68af24659706d8962a45514bfd995f15
AdamWarlord <anuragparcha315@gmail.com>
2020-10-05 22:03:47 +0530

    Added Backend Functional Dashboard

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,54 @@
+const { json } = require('body-parser');
 const express = require('express');
 const router = express.Router();
+const users = require('../Users');
+const posts = require('../Posts');
+
 
 router.get('/home', (req, res) => res.render('home'));
 router.get('/login', (req, res) => res.render('login'));
 router.get('/sign-up', (req, res) => res.render('signup'));
 router.get('/photogallery', (req, res) => res.render('gallery'));
 router.get('/dashboard', (req, res) => res.render('dashboard'));
+
+// Route to get all users from hard coded database
+router.get('/allusers', (req, res) => res.json(users));
+
+router.get('/dashboard/:id', (req, res) => {
+    const found = users.some(user => user.id === parseInt(req.params.id));
+
+    if (found) {
+        users.forEach(user => {
+            if (user.id === parseInt(req.params.id)) {
+                usersusername = user.username;
+                userthreads = user.threads;
+            }
+        });
+        const upvoteArray = [];
+        posts.forEach(post => {
+            upvoteArray.push(post.upvotes);
+        });
+        upvoteArray.sort((a, b) => b - a);
+
+        const hotposts = [];
+        upvoteArray.forEach(upvote => {
+            posts.forEach(post => {
+                if (upvote === post.upvotes) {
+                    hotposts.push(post.title);
+                }
+            })
+        });
+
+
+        res.render('dashboard', {
+            usersusername,
+            userthreads: userthreads,
+            hotposts: hotposts
+        });
+    } else {
+        res.status(404).render('404');
+    }
+});
 
 
 
