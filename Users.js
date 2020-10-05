a324d5e4b600ff63fac033d9ac8e1c657ae44627
AdamWarlord <anuragparcha315@gmail.com>
2020-10-05 11:07:47 +0530

    Added Backend Server with Node.js

---
+++ Users.js
@@ -0,0 +1,10 @@
+const users = [{
+    id: 1,
+    username: "admin",
+    password: "anurag",
+    firstName: "admin",
+    lastName: "",
+    email: "admin@bitspace.com"
+}];
+
+module.exports = users;
