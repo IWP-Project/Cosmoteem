90e791509264c11f28cebfe563d02f93f7373448
AdamWarlord <anuragparcha315@gmail.com>
2020-10-31 18:17:02 +0530

    routing new ui

--- routes/index.js
+++ routes/index.js
@@ -38,7 +38,36 @@ router.get('/home', (req, res) => res.render('home', { layout: 'home_layouts' })
 router.get('/photogallery', (req, res) => res.render('gallery'));
 router.get('/store', (req, res) => res.render('store'));
 router.get('/topnews', (req, res) => res.render('topnews'));
-router.get('/forums', (req, res) => res.render('forums'));
+
+router.get('/news/headlines', async(req, res) => {
+    res.render('news/headlines')
+})
+
+router.get('/news/article', async(req, res) => {
+    res.render('news/fullarticle')
+})
+
+router.get('/forums', async(req, res) => {
+    const cposts = await Post.find({ tags: 'community' }).populate('author').sort({ voteScore: 'desc' }).limit(5).lean()
+    const aposts = await Post.find({ tags: 'astronomy' }).populate('author').sort({ voteScore: 'desc' }).limit(5).lean()
+    const gposts = await Post.find({ tags: 'gaming' }).populate('author').sort({ voteScore: 'desc' }).limit(5).lean()
+    const fposts = await Post.find({ tags: 'faq' }).populate('author').sort({ voteScore: 'desc' }).limit(5).lean()
+    const eposts = await Post.find({ tags: 'equipment' }).populate('author').sort({ voteScore: 'desc' }).limit(5).lean()
+    const users = await User.find({}).lean()
+    const posts = await Post.find({}).lean()
+    const ulen = users.length
+    const plen = posts.length
+    res.render('forums', {
+        cposts,
+        aposts,
+        gposts,
+        fposts,
+        eposts,
+        users,
+        ulen,
+        plen
+    })
+});
 
 // Testing Purpose for handlebars
 router.get('/test/createathread', (req, res) => {
